c39500e4fc040737369968b8f4e5a5b13f0d87fa
atomu21 <a2ki1ya0ma6@gmail.com>
2022-03-09 15:39:41 +0900

    add new file

---
+++ memo.rb
@@ -0,0 +1,23 @@
+require "csv"
+puts "1(新規でメモを作成)2(メモを編集する)"
+memo_type = gets.to_s.chomp
+    if memo_type == "1"
+        puts "拡張子を除いたファイルを入力していください。"
+        file_name = gets.chomp
+        puts "メモしたい内容を記入してください"
+        puts "完了したらCtrl + Dをおします"
+        CSV.open("#{file_name}.csv", "w") do |memo|
+        content = readlines
+        memo << content
+        end
+
+    elsif memo_type == "2"
+        puts "編集したいファイル名を編集したいファイル名を入力してください"
+        file_name = gets.chomp
+        puts "編集内容を入力してください。"
+        puts "完了したらCtrl + Dをおします"
+        CSV.open("#{file_name}.csv", "w") do |memo|
+        content = readlines
+        memo << content
+        end
+    end
